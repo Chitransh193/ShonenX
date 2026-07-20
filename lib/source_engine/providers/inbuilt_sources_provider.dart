7a8ebd21bfd8636d30c42610d323297bfe7fa37c
Roshan <roshan.codespace@gmail.com>
2026-07-20 23:47:01 +0530

    Update inbuilt_sources_provider.dart

--- lib/source_engine/providers/inbuilt_sources_provider.dart
+++ lib/source_engine/providers/inbuilt_sources_provider.dart
@@ -3,13 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shonenx/core/network/http_client.dart';
 import 'package:shonenx/shared/providers/storage_provider.dart';
 import 'package:shonenx/core/remote_config/providers/remote_config_provider.dart';
-import 'package:shonenx/source_engine/dsl_engine/dsl_media_source.dart';
 import 'package:shonenx/source_engine/inbuilt_sources/anime/anidb_source.dart';
 import 'package:shonenx/source_engine/inbuilt_sources/anime/gojo_source.dart';
 import 'package:shonenx/source_engine/inbuilt_sources/anime/anikoto_source.dart';
 import 'package:shonenx/source_engine/providers/anime_source.dart';
 import 'package:shonenx/source_engine/providers/manga_source.dart';
-import 'package:shonenx/source_engine/dsl_engine/dsl_provider_service.dart';
 
 final _inbuiltAnimeListProvider = Provider<List<AnimeSource>>((ref) {
   final client = ref.watch(httpClientProvider);
@@ -22,39 +20,14 @@ final _inbuiltAnimeListProvider = Provider<List<AnimeSource>>((ref) {
 });
 
 final inbuiltAnimeSourcesProvider = Provider<List<AnimeSource>>((ref) {
-  final providersAsync = ref.watch(dslProvidersProvider);
   final inbuilt = ref.watch(_inbuiltAnimeListProvider);
-  final runtime = ref.watch(dslRuntimeProvider);
   final remoteConfig = ref.watch(remoteConfigServiceProvider);
 
-  final dslSources = providersAsync.maybeWhen(
-    data: (map) => map.values
-        .where((j) => j['mediaType']?.toString().toUpperCase() == 'ANIME')
-        .map((j) => DSLAnimeSource(providerDef: j, runtime: runtime))
-        .toList(),
-    orElse: () => <AnimeSource>[],
-  );
-
-  return [
-    ...inbuilt,
-    ...dslSources,
-  ].where((s) => !remoteConfig.isSourceDisabled(s.sourceInfo.id)).toList();
+  return inbuilt
+      .where((s) => !remoteConfig.isSourceDisabled(s.sourceInfo.id))
+      .toList();
 });
 
 final inbuiltMangaSourcesProvider = Provider<List<MangaSource>>((ref) {
-  final providersAsync = ref.watch(dslProvidersProvider);
-  final runtime = ref.watch(dslRuntimeProvider);
-  final remoteConfig = ref.watch(remoteConfigServiceProvider);
-
-  final dslSources = providersAsync.maybeWhen(
-    data: (map) => map.values
-        .where((j) => j['mediaType']?.toString().toUpperCase() == 'MANGA')
-        .map((j) => DSLMangaSource(providerDef: j, runtime: runtime))
-        .toList(),
-    orElse: () => <MangaSource>[],
-  );
-
-  return dslSources
-      .where((s) => !remoteConfig.isSourceDisabled(s.sourceInfo.id))
-      .toList();
+  return <MangaSource>[];
 });
