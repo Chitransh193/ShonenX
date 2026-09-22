c44e3057f238b57f80f7b73a4c35016f051bbab7
Roshan <roshan.codespace@gmail.com>
2026-09-23 04:11:37 +0530

    Update inbuilt_sources_provider.dart

--- lib/source_engine/providers/inbuilt_sources_provider.dart
+++ lib/source_engine/providers/inbuilt_sources_provider.dart
@@ -3,9 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shonenx/core/network/http_client.dart';
 import 'package:shonenx/shared/providers/storage_provider.dart';
 import 'package:shonenx/core/remote_config/providers/remote_config_provider.dart';
-import 'package:shonenx/source_engine/inbuilt_sources/anime/anidb_source.dart';
-import 'package:shonenx/source_engine/inbuilt_sources/anime/gojo_source.dart';
-import 'package:shonenx/source_engine/inbuilt_sources/anime/anikoto_source.dart';
+import 'package:shonenx/source_engine/inbuilt_sources/anime/anikoto.dart';
+import 'package:shonenx/source_engine/inbuilt_sources/anime/hianime.dart';
+import 'package:shonenx/source_engine/inbuilt_sources/anime/special.dart';
 import 'package:shonenx/source_engine/providers/anime_source.dart';
 import 'package:shonenx/source_engine/providers/manga_source.dart';
 
@@ -13,9 +13,9 @@ final _inbuiltAnimeListProvider = Provider<List<AnimeSource>>((ref) {
   final client = ref.watch(httpClientProvider);
   final storage = ref.watch(sharedPreferencesProvider);
   return [
-    GojoSource(client: client, storage: storage),
-    AnidbSource(client: client, storage: storage),
     AnikotoSource(client: client, storage: storage),
+    SpecialSource(client: client),
+    HiAnimeSource(client: client),
   ];
 });
 
